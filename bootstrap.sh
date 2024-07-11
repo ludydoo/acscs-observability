fa01195e392cca6cff18799868e19007082a6ba2
Ludovic Cleroux <ludydoo@gmail.com>
2024-07-11 13:30:28 +0200

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+oc label ns "openshift-monitoring" argocd.argoproj.io/managed-by=openshift-gitops
+
+oc apply -f - <<EOF
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: bootstrap
+  namespace: openshift-gitops
+spec:
+  project: default
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+  source:
+    repoURL: https://github.com/ludydoo/acscs-observability
+    targetRevision: HEAD
+    path: project
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: openshift-gitops
+EOF
