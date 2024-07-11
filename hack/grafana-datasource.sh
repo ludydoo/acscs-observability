014c0a1db0aace3a2379d8734426b54e649bab47
Ludovic Cleroux <ludydoo@gmail.com>
2024-07-11 15:57:10 +0200

    grafana operator

--- hack/grafana-datasource.sh
+++ hack/grafana-datasource.sh
@@ -3,24 +3,28 @@
 BEARER_TOKEN="$(oc create token grafana-instance-sa --duration=8760h -n grafana)"
 
 cat <<EOF | kubectl apply -f-
-apiVersion: integreatly.org/v1alpha1
-kind: GrafanaDataSource
+apiVersion: grafana.integreatly.org/v1beta1
+kind: GrafanaDatasource
 metadata:
   name: prometheus
   namespace: grafana
 spec:
-  datasources:
-    - access: proxy
-      editable: true
-      isDefault: true
-      jsonData:
-        httpHeaderName1: 'Authorization'
-        timeInterval: 5s
-        tlsSkipVerify: true
-      name: Prometheus
-      secureJsonData:
-        httpHeaderValue1: 'Bearer ${BEARER_TOKEN}'
-      type: prometheus
-      url: 'https://thanos-querier.openshift-monitoring.svc.cluster.local:9091'
-  name: prometheus-grafanadatasource.yaml
+  instanceSelector:
+    matchLabels:
+      dashboards: grafana
+  datasource:
+    access: proxy
+    editable: true
+    isDefault: true
+    jsonData:
+      httpHeaderName1: 'Authorization'
+      timeInterval: 5s
+      tlsSkipVerify: true
+      prometheusType: "Thanos"
+      customQueryParameters: "dedup=true"
+    name: Prometheus
+    secureJsonData:
+      httpHeaderValue1: 'Bearer ${BEARER_TOKEN}'
+    type: prometheus
+    url: 'https://thanos-querier.openshift-monitoring.svc.cluster.local:9091'
 EOF
